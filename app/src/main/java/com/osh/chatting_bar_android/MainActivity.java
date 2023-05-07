9be72051f6f5109b1559aa3bc4ef0c409072c0c7
OhSiHyeon <osh000308@gmail.com>
2023-05-07 15:07:34 +0900

    [FEAT]: Hello World
    [FEAT]: 안드로이드 프로젝트 생성

---
+++ app/src/main/java/com/osh/chatting_bar_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.osh.chatting_bar_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
