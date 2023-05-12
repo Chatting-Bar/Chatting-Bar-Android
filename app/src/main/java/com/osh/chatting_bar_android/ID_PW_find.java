e5da5821736f61ea88f2cfad370b41df9b96b478
OhSiHyeon <osh000308@gmail.com>
2023-05-12 16:19:10 +0900

    [FEAT] 아이디/비번 찾기 UI 배치

---
+++ app/src/main/java/com/osh/chatting_bar_android/ID_PW_find.java
@@ -0,0 +1,14 @@
+package com.osh.chatting_bar_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ID_PW_find extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_id_pw_find);
+    }
+}
