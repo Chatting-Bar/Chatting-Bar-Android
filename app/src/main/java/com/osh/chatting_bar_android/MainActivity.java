97c05e51213abafe617df2e1d7b101623d73e884
OhSiHyeon <osh000308@gmail.com>
2023-05-12 15:23:47 +0900

    [FEAT]: 로그인 화면 완벽 구성

--- app/src/main/java/com/osh/chatting_bar_android/MainActivity.java
+++ app/src/main/java/com/osh/chatting_bar_android/MainActivity.java
@@ -1,14 +1,13 @@
 package com.osh.chatting_bar_android;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
