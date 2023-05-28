2a6bf8872a3c737c7efc69d364921c52c6927ebc
OhSiHyeon <osh000308@gmail.com>
2023-05-29 07:44:00 +0900

    [FEAT] 스플레시에서 로그인 토큰 검사

--- app/src/main/java/com/osh/chatting_bar_android/Splash.java
+++ app/src/main/java/com/osh/chatting_bar_android/Splash.java
@@ -2,16 +2,21 @@ package com.osh.chatting_bar_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        pref = getSharedPreferences("user", Activity.MODE_PRIVATE);
 
         moveMain(1);
     }
@@ -22,12 +27,20 @@ public class Splash extends AppCompatActivity {
             @Override
             public void run()
             {
-                //new Intent(현재 context, 이동할 activity)
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                if (pref.getString("Token", "").isEmpty()) { //토큰이 없으면 로그인 화면으로 전환
+                    //new Intent(현재 context, 이동할 activity)
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
 
-                startActivity(intent);	//intent 에 명시된 액티비티로 이동
+                    startActivity(intent);    //intent 에 명시된 액티비티로 이동
 
-                finish();	//현재 액티비티 종료
+                    finish();    //현재 액티비티 종료
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                    startActivity(intent);    //intent 에 명시된 액티비티로 이동
+
+                    finish();    //현재 액티비티 종료
+                }
             }
         }, 1000 * sec); // sec초 정도 딜레이를 준 후 시작
     }
