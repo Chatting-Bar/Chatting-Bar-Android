3a735e8c561af5188b1a3f260c7da9e7807d15b4
OhSiHyeon <osh000308@gmail.com>
2023-05-29 13:51:11 +0900

    [FEAT] 화면전환 끝

--- app/src/main/java/com/osh/chatting_bar_android/SearchActivity.java
+++ app/src/main/java/com/osh/chatting_bar_android/SearchActivity.java
@@ -1,6 +1,9 @@
 package com.osh.chatting_bar_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,22 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        InitBtn();
+    }
+
+    protected void InitBtn()
+    {
+        //채팅 기록
+        Button exit_btn = findViewById(R.id.Exit_button);
+        exit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/osh/chatting_bar_android/SettingActivity.java
+++ app/src/main/java/com/osh/chatting_bar_android/SettingActivity.java
@@ -2,7 +2,10 @@ package com.osh.chatting_bar_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +13,55 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        InitBtn();
+    }
+
+    protected void InitBtn()
+    {
+        //채팅 기록
+        Button chatlist_btn = findViewById(R.id.chattingList_button);
+        chatlist_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ChatLogActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+        //홈
+        Button home_btn = findViewById(R.id.on_home_button);
+        home_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+        //검색
+        Button search_btn = findViewById(R.id.search_button);
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+        //설정
+        Button setting_btn = findViewById(R.id.setting_button);
+        setting_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
     }
 }
