6646de15482dbb65ae72d1e4bc84d866db68db03
OhSiHyeon <osh000308@gmail.com>
2023-05-30 23:39:21 +0900

    [FEAT] 검색 기능 추가
    - 검색하면 메인으로 값 넘어감(Tosat로 나옴)
    - 검색어 기록은 최신순으로 나옴
    - 검색어 기록 클릭해도 값이 메인으로 넘어가고 기록도 최신화됨

--- app/src/main/java/com/osh/chatting_bar_android/MainActivity.java
+++ app/src/main/java/com/osh/chatting_bar_android/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        if (intent.getStringExtra("search") != null) {
+            Toast.makeText(getApplicationContext(), intent.getStringExtra("search"), Toast.LENGTH_SHORT).show();
+        }
 
         InitBtn();
     }
